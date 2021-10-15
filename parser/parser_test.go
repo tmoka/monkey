129d2f757e21303f2e2de7806291a45fa5ec592e
tmoka0476 <knowledzr+alt@gmail.com>
2021-10-15 22:24:21 +0900

    Correct typo

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -252,7 +252,7 @@ func testLiteralExpression(t *testing.T, exp ast.Expression, expected interface{
 	return false
 }
 
-func TestParsingInfixExpression(t *testing.T, exp ast.Expression, left interface{}, operator string, right interface{}) bool {
+func testInfixExpression(t *testing.T, exp ast.Expression, left interface{}, operator string, right interface{}) bool {
 	opExp, ok := exp.(*ast.InfixExpression)
 	if !ok {
 		t.Errorf("exp os not ast.InfixExpression. got=%T(%s)", exp, exp)
