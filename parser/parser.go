bda08eaecf9d66e8937a29e96c8d947132bdbcae
tmoka0476 <knowledzr+alt@gmail.com>
2022-05-25 15:11:58 +0900

    Add parseGroupedExpression

--- parser/parser.go
+++ parser/parser.go
@@ -69,6 +69,8 @@ func New(l *lexer.Lexer) *Parser {
 	p.registerInfix(token.LT, p.parseInfixExpression)
 	p.registerInfix(token.GT, p.parseInfixExpression)
 
+	p.registerPrefix(token.LPAREN, p.parseGroupedExpression)
+
 	p.nextToken()
 	p.nextToken()
 
@@ -276,4 +278,15 @@ func (p *Parser) parseInfixExpression(left ast.Expression) ast.Expression {
 
 func (p *Parser) parseBoolean() ast.Expression {
 	return &ast.Boolean{Token: p.curToken, Value: p.curTokenIs(token.TRUE)}
+}
+
+func (p *Parser) parseGroupedExpression() ast.Expression {
+	p.nextToken()
+
+	exp := p.parseExpression(LOWEST)
+	if !p.expectPeek(token.RPAREN) {
+		return nil
+	}
+
+	return exp
 }
